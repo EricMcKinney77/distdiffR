f704340a697fb487551a5a26f2020dfc87532513
Eric McKinney <ericmckinney77@gmail.com>
2021-01-05 14:21:00 -0700

    Initial commit

---
+++ R/ToroShiftDataRcppFun.R
@@ -0,0 +1,22 @@
+cppFunction('
+  List ToroShiftData(NumericMatrix data, int n1, int n2, float propPnts = 1) {
+      NumericMatrix::Column X = data( _, 0);
+      NumericMatrix::Column Y = data( _, 1);
+      float dataWidth = max(X) - min(X);
+      float dataHeight = max(Y) - min(Y);
+      
+      int n1plusn2 = n1 + n2;
+      int numShifts = round(propPnts * n1plusn2);
+      List shiftDataList = List::create();
+      IntegerVector n1n2vec = seq(0, n1plusn2 - 1);
+      IntegerVector sampledRows = sample(n1n2vec, numShifts);
+      for (int x = 0; x < numShifts; ++x) {
+        int sampledRow = sampledRows[x];
+        NumericVector toroCntrPnt = data(sampledRow, _);
+        NumericVector shiftedX = ifelse(X < toroCntrPnt[0], X + dataWidth, X);
+        NumericVector shiftedY = ifelse(Y < toroCntrPnt[1], Y + dataHeight, Y);
+        shiftDataList.push_back(cbind(shiftedX, shiftedY));
+      }
+      return shiftDataList;
+  }
+')
