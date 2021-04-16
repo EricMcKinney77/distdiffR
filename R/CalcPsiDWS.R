25b6aaa232fc958f54da625ae05fee5ee9c0346e
EricMcKinney77 <ericmckinney77@gmail.com>
2021-04-15 23:03:10 -0600

    Initial commit using Hadleys workflow

---
+++ R/CalcPsiDWS.R
@@ -0,0 +1,8 @@
+CalcPsiDWS <- function(data, subjects) {
+  data1 <- data[subjects == 1, ]
+  data2 <- data[subjects == 2, ]
+  n1 <- nrow(data1)
+  n2 <- nrow(data2)
+  (n1 * sum((bcdf(data1, data1) - bcdf(data2, data1))^2) +
+      n2 * sum((bcdf(data2, data2) - bcdf(data1, data2))^2)) / (n1 + n2)
+}
