5d266c8bfa0a3c6170f7b5d69f9d032aa431ee06
EricMcKinney77 <ericmckinney77@gmail.com>
2021-12-27 18:46:18 -0700

    Updated distdiffRtest.R to only use distdiffr()

--- distdiffRtest.R
+++ distdiffRtest.R
@@ -15,44 +15,48 @@ data1 <- as.matrix(iris[1:75, -(3:5)])
 data2 <- as.matrix(iris[76:150, -(3:5)])
 
 # Rotational test
-output <- BivarDistDiffTest(data1,
-                            data2,
-                            seedNum = seedNum)
+output <- distdiffr(data1,
+                    data2,
+                    testType = "rotational",
+                    seedNum = seedNum)
 output$pval == 0.561
 
 # Toroidal shift test with proportions of points
-output <- BivarToroDiffTest(data1,
-                            data2,
-                            propPnts = 0.1,
-                            seedNum = seedNum)
+output <- distdiffr(data1,
+                    data2,
+                    testType = "toroidal",
+                    propPnts = 0.1,
+                    seedNum = seedNum)
 output$pval == 0.566
 
 # Toroidal shift test with thresholds below pooled sample size
-output <- BivarToroDiffTest(data1,
-                            data2,
-                            shiftThrshld = 25,
-                            seedNum = seedNum)
+output <- distdiffr(data1,
+                    data2,
+                    testType = "toroidal",
+                    seedNum = seedNum)
 output$pval == 0.477
 
 # Toroidal shift test with thresholds above pooled sample size
-output <- BivarToroDiffTest(data1,
-                            data2,
-                            shiftThrshld = 100,
-                            seedNum = seedNum)
+output <- distdiffr(data1,
+                    data2,
+                    testType = "toroidal",
+                    shiftThrshld = 100,
+                    seedNum = seedNum)
 output$pval == 0.502
 
 # Toroidal shift test with a number of shifts
-output <- BivarToroDiffTest(data1,
-                            data2,
-                            numShifts = 8,
-                            seedNum = seedNum)
+output <- distdiffr(data1,
+                    data2,
+                    testType = "toroidal",
+                    numShifts = 8,
+                    seedNum = seedNum)
 output$pval == 0.648
 
 # Combined rotational and toroidal shift test
-output <- RotateToroDiffTest(data1,
-                             data2,
-                             shiftThrshld = 25,
-                             seedNum = seedNum)
+output <- distdiffr(data1,
+                    data2,
+                    testType = "combined",
+                    seedNum = seedNum)
 output$pval == 0.307
 
 
@@ -62,42 +66,46 @@ data1 <- as.matrix(iris[iris[5] == "setosa", -(3:5)])
 data2 <- as.matrix(iris[iris[5] == "virginica", -(3:5)])
 
 # Rotational test
-output <- BivarDistDiffTest(data1,
-                            data2,
-                            seedNum = seedNum)
+output <- distdiffr(data1,
+                    data2,
+                    testType = "rotational",
+                    seedNum = seedNum)
 output$pval == 0.001
 
 # Toroidal shift test with proportions of points
-output <- BivarToroDiffTest(data1,
-                            data2,
-                            propPnts = 0.1,
-                            seedNum = seedNum)
+output <- distdiffr(data1,
+                    data2,
+                    testType = "toroidal",
+                    propPnts = 0.1,
+                    seedNum = seedNum)
 output$pval == 0.001
 
 # Toroidal shift test with thresholds below pooled sample size
-output <- BivarToroDiffTest(data1,
-                            data2,
-                            shiftThrshld = 25,
-                            seedNum = seedNum)
+output <- distdiffr(data1,
+                    data2,
+                    testType = "toroidal",
+                    seedNum = seedNum)
 output$pval == 0.001
 
 # Toroidal shift test with thresholds above pooled sample size
-output <- BivarToroDiffTest(data1,
-                            data2,
-                            shiftThrshld = 100,
-                            seedNum = seedNum)
+output <- distdiffr(data1,
+                    data2,
+                    testType = "toroidal",
+                    shiftThrshld = 100,
+                    seedNum = seedNum)
 output$pval == 0.001
 
 # Toroidal shift test with a number of shifts
-output <- BivarToroDiffTest(data1,
-                            data2,
-                            numShifts = 8,
-                            seedNum = seedNum)
+output <- distdiffr(data1,
+                    data2,
+                    testType = "toroidal",
+                    numShifts = 8,
+                    seedNum = seedNum)
 output$pval == 0.001
 
 # Combined rotational and toroidal shift test
-output <- RotateToroDiffTest(data1,
-                             data2,
-                             shiftThrshld = 25,
-                             seedNum = seedNum)
+output <- distdiffr(data1,
+                    data2,
+                    testType = "combined",
+                    seedNum = seedNum)
 output$pval == 0.001
